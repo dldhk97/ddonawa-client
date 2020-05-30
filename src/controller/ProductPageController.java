2bf6537176efe0e55a2810fe3dfb6a6e0c7ff1ce
gg631 <gg631@DESKTOP-SPS26KJ>
2020-05-30 21:20:49 +0900

    메인화면으로 가는 버튼 추가

--- src/controller/ProductPageController.java
+++ src/controller/ProductPageController.java
@@ -5,15 +5,26 @@ import java.net.URI;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Popup;
+import javafx.stage.Stage;
 import javafx.stage.Window;
+import javafx.util.Duration;
+import utility.IOHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -34,6 +45,68 @@ public class ProductPageController implements Initializable {
 
 	    @FXML
 	    private Button zzimRegister;
+	    
+	    @FXML
+	    private Button goToMainBtn;
+
+	    @FXML
+	    void OnGoToMainBtnClicked(ActionEvent event) {
+	    	 try {
+	             //메인페이지로 이동하기
+	             Stage primaryStage = (Stage) Image.getScene().getWindow();
+	             Parent root = FXMLLoader.load(getClass().getResource("/page/MainPage.fxml"));
+	             //Scene scene = new Scene(root);    	    
+	            	
+	             Pane root1 = new Pane();
+	        	 	root1.setPrefSize(600, 400);    
+	        	 	VBox menu = new VBox();
+	        	 
+	        	    menu.setId("menu");
+	        	    menu.prefHeightProperty().bind(root1.heightProperty());
+	        	    menu.setPrefWidth(100);
+
+	        	   // 나중에 동적으로 추가해줘야 할듯
+	        	    MenuItem menuItem1 = new MenuItem("A-1");
+	        	    MenuItem menuItem2 = new MenuItem("A-2");
+	        	 	MenuItem menuItem3 = new MenuItem("A-3");
+	        	 	MenuButton menuBtn1= new MenuButton("카테고리 A", null,menuItem1,menuItem2,menuItem3);
+	        	 	
+	        	 	MenuItem menuItem4 = new MenuItem("B-1");
+	     	    MenuItem menuItem5 = new MenuItem("B-2");
+	     	 	MenuItem menuItem6 = new MenuItem("B-3");
+	     	 	MenuButton menuBtn2= new MenuButton("카테고리 B", null,menuItem4,menuItem5,menuItem6);
+	        	 	menu.getChildren().addAll(menuBtn1,menuBtn2);
+
+	        	    menu.getStylesheets().add(getClass().getResource("/application/menustyle.css").toExternalForm());
+	        	    menu.setTranslateX(-90);
+	        	    TranslateTransition menuTranslation = new TranslateTransition(Duration.millis(500), menu);
+
+	        	    menuTranslation.setFromX(-90);
+	        	    menuTranslation.setToX(0);
+
+	        	    menu.setOnMouseEntered(evt -> {
+	        	        menuTranslation.setRate(1);
+	        	        menuTranslation.play();
+	        	    });
+	        	    menu.setOnMouseExited(evt -> {
+	        	        menuTranslation.setRate(-1);
+	        	        menuTranslation.play();
+	        	    });
+	        	    
+	        	    root1.getChildren().addAll(root,menu);
+	        	    Scene scene = new Scene(root1);
+	        	    
+	        	    
+	             primaryStage.setScene(scene);
+	             primaryStage.setTitle("또나와 메인화면");
+	             primaryStage.show();
+
+	         } catch (Exception e) {
+	         	String errorMsg = "LoginPageController.moveToMain\n" + e.getMessage();
+	         	IOHandler.getInstance().showAlert(errorMsg);
+	         	IOHandler.getInstance().log(errorMsg);
+	         }
+	    }
     Popup popup;
     TextArea textArea;
     
